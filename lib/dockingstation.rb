dada4bf424f4c56660c213d54277332702cd4bcc
Vicky Ledsom <victorialedsom@gmail.com>
2017-02-15 18:03:23 +0000

    Complete up to step 11

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,4 +1,17 @@
+require_relative 'bike'
+
 class DockingStation
+  attr_reader :bike
+
+  def initialize
+    @bike = Bike.new
+  end
+
   def release_bike
+    Bike.new
+  end
+
+  def dock(outside_bike)
+    @bike = outside_bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,17 @@
-require "dockingstation"
+require 'dockingstation'
 
 describe DockingStation do
-it {is_expected.to respond_to(:release_bike)}
+  let(:dockingstation) {described_class.new}
+  it {is_expected.to respond_to(:release_bike)}
+
+  it 'checks if bike is working' do
+    expect(dockingstation.release_bike).to be_working
+  end
+
+  it {is_expected.to respond_to(:dock).with(1).argument}
+
+  it 'Checks if a bike is returned' do
+    outside_bike = Bike.new
+    expect(dockingstation.dock(outside_bike)).to eq outside_bike
+  end
 end
