853b5c028cfd355d338e5e15bff4a2500cffa543
Vicky Ledsom <victorialedsom@gmail.com>
2017-02-16 17:09:17 +0000

    Add capacity feature to the dock

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,21 +1,27 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike
+  attr_reader :bike, :dock_capacity
 
   def initialize
     @bike_counter = 1
+    @dock_capacity = 1
   end
 
   def release_bike
-    fail "No bikes are avaliable" unless @bike_counter > 0
-      @bike = Bike.new
+    raise "No bikes are avaliable" unless @bike_counter > 0
+      @bike_2 = Bike.new
       @bike_counter  =- 1
-      return @bike
+      return @bike_2
   end
 
   def dock(outside_bike)
-    @bike = outside_bike
+    if @bike_counter >= @dock_capacity
+      fail "The dock is full"
+    else
+      @bike = outside_bike
+      @bike_counter =+ 1
+      return @bike
+    end
   end
-
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,16 +8,28 @@ describe DockingStation do
     expect(dockingstation.release_bike.working?).to eq true
   end
 
+  it 'releases a bike' do
+    expect(dockingstation.release_bike).to be_a Bike
+  end
+
   it {is_expected.to respond_to(:dock).with(1).argument}
 
   it 'Checks if a bike is returned' do
-    outside_bike = Bike.new
-    expect(dockingstation.dock(outside_bike)).to eq outside_bike
+    # outside_bike = Bike.new
+    bike = dockingstation.release_bike
+    expect(dockingstation.dock(bike)).to eq bike
   end
 
   it 'wont release a bike if there are none avaliable' do
     #I expect it to raise an error if there are no bikes avaliable
     dockingstation.release_bike
     expect{dockingstation.release_bike}.to raise_error("No bikes are avaliable")
+  end
+
+  it 'wont dock the bike if the dock is full' do
+    # bike is a brand new bike, which we are passing into out socking station. As we haven't
+    # taken a bike out of the docking station, it won't fit as we can only fit one bike in the docking station.
+    bike = Bike.new
+    expect{dockingstation.dock(bike)}.to raise_error("The dock is full")
   end
 end
